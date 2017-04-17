f5720d6842471eff1571696e7248a91cad45bc8e
Alex Vargas <ravargasaco@miners.utep.edu>
2017-04-17 15:01:06 -0600

    no message

--- cleanReview.sh
+++ cleanReview.sh
@@ -1,1 +1,1 @@
-jq 'del(.user_id, .stars, .date, .useful, .funny, .cool, .type, .review_id)' reviews.json > new.json
+jq 'del(.user_id, .stars, .useful, .funny, .cool, .type, .review_id)' reviews.json > new.json
