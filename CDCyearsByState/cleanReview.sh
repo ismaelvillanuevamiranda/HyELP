bf3da4db8179f4a0bd941fde474b9a62668d3492
Alex Vargas <ravargasaco@miners.utep.edu>
2017-04-21 13:08:40 -0600

    Added scripts to filter reviews by periods of year according to CDC.

--- CDCyearsByState/cleanReview.sh
+++ CDCyearsByState/cleanReview.sh
@@ -1,2 +1,7 @@
 jq 'del(.user_id, .stars, .useful, .funny, .cool, .type, .review_id)' reviews.json > new.json
-jq 'select((.date >= "2014-01-01" and .date <= "2014-12-01"))' new.json | jq '.business_id +"***"+ .text'
+jq 'select((.date >= "1998-01-01" and .date <= "1998-12-31") or 
+(.date >= "2001-01-01" and .date <= "2006-12-31") or 
+(.date >= "2008-01-01" and .date <= "2015-12-31"))' new.json | jq '.business_id +"***"+ .text' > reviews1998_01_011998_12_312001_01_012006_12_312008_01_012015_12_31.txt
+jq 'select((.date >= "1998-01-01" and .date <= "2008-12-31") or 
+(.date >= "2010-01-01" and .date <= "2014-12-31"))' new.json | jq '.business_id +"***"+ .text' > reviews1998_01_012008_12_312010_01_012014_12_31.txt
+jq 'select((.date >= "1998-01-01" and .date <= "2015-12-31"))' new.json | jq '.business_id +"***"+ .text' > reviews1998_01_012015_12_31.txt
