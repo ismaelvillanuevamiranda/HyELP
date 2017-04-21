0c2db5ba256b21601596d537c6d9f28aab137a03
Alex Vargas <avargas.rava@gmail.com>
2017-04-21 13:12:45 -0600

    Make new line

--- CDCyearsByState/cleanReview.sh
+++ CDCyearsByState/cleanReview.sh
@@ -5,4 +5,4 @@ jq 'select((.date >= "1998-01-01" and .date <= "1998-12-31") or
 jq 'select((.date >= "1998-01-01" and .date <= "2008-12-31") or 
 (.date >= "2010-01-01" and .date <= "2014-12-31"))' new.json | jq '.business_id +"***"+ .text' > reviews1998_01_012008_12_312010_01_012014_12_31.txt
 jq 'select((.date >= "1998-01-01" and .date <= "2015-12-31"))' new.json | jq '.business_id +"***"+ .text' > reviews1998_01_012015_12_31.txt
- 
+  
